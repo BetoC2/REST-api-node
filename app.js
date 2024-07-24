c9b978fe23c0e011eb2e674972c82e1e83af9843
Alberto <alberto.renteriac@iteso.mx>
2024-07-24 16:54:02 -0600

    Change commonJS to ESmodules and code refactor

--- app.js
+++ app.js
@@ -1,78 +1,48 @@
-const express = require('express')
-const crypto = require('node:crypto')
-const { validateMovie, validatePartialMovie } = require('./schema')
-const movies = require('./movies.json')
+// Import dependencies
+import express from 'express'
+import cors from 'cors'
+
+// Import local functions
+import { moviesRouter } from './routes/movies'
+
+// Initialization
+const app = express()
+
+// Middlewares and routes
+app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'http://localhost:4321',
+      'https://movies.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+// Todos los recursos que sean MOVIES se identifica con /movies
+app.use('/movies', moviesRouter)
 
 const PORT = process.env.PORT ?? 1234
 
-const app = express()
-app.use(express.json())
-app.disable('x-powered-by')
-
-// Todos los recursos que sean MOVIES se identifican con el endpoint /movies
-app.get('/movies', (req, res) => {
-  const { genre } = req.query
-  if (genre) {
-    const filteredMovies = movies.filter(
-      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-    )
-    return res.json(filteredMovies)
-  }
-  res.json(movies)
-})
-
-// Crear peliculas
-app.post('/movies', (req, res) => {
-  const result = validateMovie(req.body)
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newMovie = {
-    id: crypto.randomUUID(), // UUID v4
-    ...result.data
-  }
-
-  // Esto no es rest, queda hacer la base de datos
-  movies.push(newMovie)
-  res.status(201).json(newMovie) // Actualizar caché del cliente
-})
-
-// Filtrar peliculas por id
-app.get('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movie = movies.find(movie => movie.id === id)
-
-  if (!movie) return res.status(404).json({ error: 'Movie not found' })
-
-  res.json(movie)
-})
-
-// Actualizar peliculas por id
-app.patch('/movies/:id', (req, res) => {
-  const result = validatePartialMovie(req.body)
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) return res.status(404).json({ message: 'Movie not found' })
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data
-  }
-
-  movies[movieIndex] = updateMovie
-
-  return res.json(updateMovie)
-})
-
-// App listen
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`server listening on port http://localhost:${PORT}`)
 })
